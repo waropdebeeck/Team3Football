d632beeec26bd6a5ee454e3add6170eece3e7329
unknown <rubenhoremans1999@gmail.com>
2019-11-15 11:29:52 +0100

    players-serivce

---
+++ players-service/src/main/java/be/thomasmore/playersservice/PlayersServiceApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.PlayersService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayersServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayersServiceApplication.class, args);
+	}
+
+}
