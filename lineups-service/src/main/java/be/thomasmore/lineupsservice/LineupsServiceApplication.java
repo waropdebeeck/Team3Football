9d588af4f2079bda88b2a986f407daa572d9904c
melih <melih.91@hotmail.com>
2019-11-15 22:41:19 +0100

    lineup

---
+++ lineups-service/src/main/java/be/thomasmore/lineupsservice/LineupsServiceApplication.java
@@ -0,0 +1,13 @@
+package be.thomasmore.LineupsService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LineupsServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LineupsServiceApplication.class, args);
+	}
+
+}
---
+++ lineups-service/src/test/java/be/thomasmore/LineupsService/LineupServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package be.thomasmore.LineupsService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LineupsServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
